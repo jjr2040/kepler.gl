5a0d98364a623d009066907384e1b076827e06aa
Juan Jose Villegas <juan.villegas@rappi.com>
2020-12-12 15:25:56 -0500

    Added json and style files for loading datasets

---
+++ src/components/side-panel/custom-panel-style.js
@@ -0,0 +1,18 @@
+
+const style = {
+    container: {
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    button: {
+        selected: {
+            backgroundColor: 'blue',
+        },
+        backgroundColor: 'white'
+    },
+    title: {
+        color: 'red'
+    }
+};
+
+export default style;
