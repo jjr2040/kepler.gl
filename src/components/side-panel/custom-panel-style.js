8cf53ce82c7eeef9a23af76942f7a58842d77005
alevillejo <alevillejo@gmail.com>
2020-12-14 20:08:08 -0500

    Styled

--- src/components/side-panel/custom-panel-style.js
+++ src/components/side-panel/custom-panel-style.js
@@ -1,18 +1,42 @@
 
 const style = {
-    container: {
-        display: 'flex',
-        flexDirection: 'column'
+  container: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  button: {
+    selected: {
+      backgroundColor: '#f56fba',
+      border: 'none',
+      padding: '3px 32px',
+      margin: '8px 2px',
+      borderRadius: '8px',
+      cursor: 'pointer',
+      boxShadow: '0 12px 16px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.19)'
     },
-    button: {
-        selected: {
-            backgroundColor: 'blue',
-        },
-        backgroundColor: 'white'
-    },
-    title: {
-        color: 'red'
-    }
+    backgroundColor: '#D9D9D9',
+    border: 'none',
+    padding: '1px 32px',
+    margin: '8px 2px',
+    borderRadius: '8px',
+    cursor: 'pointer',
+    hoverBackgroundColor: 'white'
+  },
+  title: {
+    color: 'white'
+  },
+  removeButton: {
+    backgroundColor: '#f24bae',
+    border: 'none',
+    padding: '1px 32px',
+    margin: '18px 2px',
+    cursor: 'pointer',
+    borderRadius: '8px'
+  },
+  logo: {
+    alignSelf: 'flex-end',
+    margin: '20px 2px'
+  }
 };
 
 export default style;
--- src/components/side-panel/custom-panel.js
+++ src/components/side-panel/custom-panel.js
@@ -21,33 +21,62 @@
 import React, { useEffect, useState } from 'react';
 import { FilterFunnel } from '../common/icons';
 import style from './custom-panel-style.js';
+// import logopro from './logopro.png';
 
 // This is a dummy component that can be replaced to inject more side panel sub panels into the side bar
 function CustomPanelsFactory() {
-  
   const CustomPanels = props => {
-    
     const TAG = 1;
     const FILTER = 0;
-    
+
     const { setFilter, addFilter, removeFilter, datasets, enlargeFilter } = props;
-    const [ selectedFilters, setSelectedFilters ] = useState([]);
+    const [selectedFilters, setSelectedFilters] = useState([]);
 
     const filters = [
+      {
+        label: 'Timeline',
+        keyword: 'yearTS',
+        type: TAG
+      },
+      {
+        label: 'Architecture',
+        keyword: 'architecture',
+        type: FILTER
+      },
+      {
+        label: 'Archivism',
+        keyword: 'archivism',
+        type: FILTER
+      },
       {
         label: 'Ecology',
         keyword: 'ecology',
         type: FILTER
       },
       {
-        label: 'Social narrative',
-        keyword: 'social narrative',
+        label: 'Literature',
+        keyword: 'literature',
         type: FILTER
       },
       {
-        label: 'Timeline',
-        keyword: 'yearTS',
-        type: TAG
+        label: 'New media',
+        keyword: 'new media',
+        type: FILTER
+      },
+      {
+        label: 'Performance',
+        keyword: 'performance',
+        type: FILTER
+      },
+      {
+        label: 'Plastic visual arts',
+        keyword: 'plastic visual arts',
+        type: FILTER
+      },
+      {
+        label: 'Social narrative',
+        keyword: 'social narrative',
+        type: FILTER
       }
     ];
 
@@ -57,16 +86,16 @@ function CustomPanelsFactory() {
     }, []);
 
     useEffect(() => {
-      
-      const keywords = selectedFilters.filter( f => f.type === FILTER).map( f => f.keyword);
+      const keywords = selectedFilters.filter(f => f.type === FILTER).map(f => f.keyword);
 
       setFilter(FILTER, 'name', 'keywords');
       setFilter(FILTER, 'value', keywords);
 
-      selectedFilters.filter( f => f.type === TAG)
-                    .forEach( ({ keyword }) => {
-                      setFilter(TAG, 'name', keyword);
-      });
+      selectedFilters
+        .filter(f => f.type === TAG)
+        .forEach(({ keyword }) => {
+          setFilter(TAG, 'name', keyword);
+        });
     }, [selectedFilters]);
 
     const removeFilters = () => {
@@ -76,17 +105,17 @@ function CustomPanelsFactory() {
     };
 
     const Button = filter => {
-
       const { keyword, label, type } = filter;
-      const buttonStyle = selectedFilters.find( f => f.keyword === keyword ) ? style.button.selected : style.button;
+      const buttonStyle = selectedFilters.find(f => f.keyword === keyword)
+        ? style.button.selected
+        : style.button;
 
-      const action = () =>  {
-        
-        const filterIndex = selectedFilters.findIndex( f => f.keyword === keyword );
-        
+      const action = () => {
+        const filterIndex = selectedFilters.findIndex(f => f.keyword === keyword);
+
         if (filterIndex > -1) {
-          const newFilters = selectedFilters.filter( f => f.keyword !== keyword );
-          
+          const newFilters = selectedFilters.filter(f => f.keyword !== keyword);
+
           if (!newFilters.length) {
             removeFilters();
           } else {
@@ -102,20 +131,18 @@ function CustomPanelsFactory() {
       };
 
       return (
-        <button 
-          style={ buttonStyle } 
-          key={keyword} 
-          onClick={ action }>
-            {label}
-          </button>
+        <button style={buttonStyle} key={keyword} onClick={action}>
+          {label}
+        </button>
       );
     };
 
     return (
       <div style={style.container}>
-        <p style={style.title}>Cualquier cosa</p>
-        <button onClick={removeFilters}>Remove filters</button>
-        { filters.map( filter => Button(filter) ) }
+        <p style={style.title}>Use the timeline or choose keywords to filter the projects.</p>
+        {filters.map(filter => Button(filter))}
+        <button style={style.removeButton} onClick={removeFilters}> Remove filters </button>
+        <img style={style.logo} width="160" height="25" alt="logo" src={'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F4%2F41%2FProhelvetia_Logo.svg%2F1134px-Prohelvetia_Logo.svg.png&f=1&nofb=1'} />
       </div>
     );
   };
@@ -127,7 +154,7 @@ function CustomPanelsFactory() {
         id: 'predefined-filters',
         label: 'Filters',
         iconComponent: FilterFunnel
-      },
+      }
       // {
       //   id: 'chart',
       //   label: 'Chart',
